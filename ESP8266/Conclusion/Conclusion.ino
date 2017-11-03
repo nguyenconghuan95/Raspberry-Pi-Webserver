7951245e866bc60a640029a01f1fd499cb2b8d0a
nguyenconghuan95 <nguyenconghuan95@gmail.com>
2017-11-03 18:26:50 +0700

    Update GetControl function to receive JSON from server
    + Need a function to handle JSON get the control for device
    + Timing function problem

--- ESP8266/Conclusion/Conclusion.ino
+++ ESP8266/Conclusion/Conclusion.ino
@@ -6,17 +6,16 @@
 
 #define MeasureTaskPeriod     2400
 #define GetControlTaskPeriod  2
-<<<<<<< HEAD
+
 #define FirstZoneHumid          0x01
 #define FirstZoneTemp           0x02
 #define SecondZoneHumid          0x03
 #define SecondZoneTemp           0x04
-=======
 #define FirstZoneHumid        0x01
 #define FirstZoneTemp         0x02
 #define SecondZoneHumid       0x03
 #define SecondZoneTemp        0x04
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
+
 
 BH1750 lightMeter;
 
@@ -29,10 +28,9 @@ bool GetControlTaskFlag = 0;
 uint16_t MeasureTaskCount = 0;
 char GetControlTaskCount = 0; 
 
-<<<<<<< HEAD
-=======
+
+
 //Function timing the other functions
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
 void timing(void) 
 {
   delay(250);
@@ -46,17 +44,11 @@ void timing(void)
   }
 }
 
-<<<<<<< HEAD
-int measureHumid(char zone) {
-  if (zone == 1) {
-    digitalWrite(12, FirstZoneHumid && 0x01);
-=======
 //Functions measure the parameter
 int measureHumid(char zone) 
 {
   if (zone == 1) {
     digitalWrite(12, FirstZoneHumid && 0x01);         
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
     digitalWrite(13, (FirstZoneHumid && 0x02) >> 1);
     digitalWrite(14, (FirstZoneHumid && 0x04) >> 2); 
   }
@@ -73,12 +65,8 @@ int measureHumid(char zone)
   return percent;
 }
 
-<<<<<<< HEAD
-int measureTemp(char zone) {
-=======
 int measureTemp(char zone) 
 {
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
   if (zone == 1) {
     digitalWrite(12, FirstZoneTemp && 0x01);
     digitalWrite(13, (FirstZoneTemp && 0x02) >> 1);
@@ -95,12 +83,8 @@ int measureTemp(char zone)
   return value;
 }
 
-<<<<<<< HEAD
-uint16_t measureLux(char zone) {
-=======
 uint16_t measureLux(char zone) 
 {
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
   uint16_t lux = lightMeter.readLightLevel();
   Serial.print("Light: ");
   Serial.print(lux);
@@ -108,12 +92,8 @@ uint16_t measureLux(char zone)
   return lux;
 }
 
-<<<<<<< HEAD
-void passValues(char zone, int humid, uint16_t lux, int temp) {
-=======
 void passValues(char zone, int humid, uint16_t lux, int temp) 
 {
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
   Serial.print("Ket noi toi web ");
   Serial.println(host);
   WiFiClient client;
@@ -136,7 +116,6 @@ void GetControl()
   }
   String url = "/getControl.php";
   client.print(String("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n"));
-<<<<<<< HEAD
   delay(100);
   while (client.available()) {
     String line = client.readStringUntil('\r');
@@ -144,11 +123,6 @@ void GetControl()
       String data = line;
       Serial.println(data);
     }
-=======
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.println(line); 
->>>>>>> 2058f51931f21255492bc5495b8d522815aad887
   }
 }
 
