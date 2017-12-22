a618f082a8c2da5c91c590000fb486705963d08d
nguyenconghuan95 <nguyenconghuan95@gmail.com>
2017-12-22 08:47:24 +0700

    [Fixed Bug]: Fixed 'dataControl array didn't reset'

--- ESP8266/TickerTest/ControlTask.ino
+++ ESP8266/TickerTest/ControlTask.ino
@@ -1,3 +1,37 @@
+void DoControlFirstTime()
+{
+  String data;
+  int i;
+  Serial.print("Ket noi toi web: ");
+  Serial.println(host);
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Khong the ket noi toi " + String(host));
+  }
+  String url = "/getControl.php?first";
+  client.print(String("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n"));
+  delay(100);
+  while (client.available()) {
+    String line = client.readStringUntil('\n');
+    if (String(line[0]) == String("[")) {
+      data = line;
+      Serial.println(data);
+    }
+  }
+  memset(dataControl, '\0', 10);
+  parseJson(data, '{', '}');
+  
+  for (i=0; i<10 && dataControl[i].length() > 2; i++) {
+    JsonObject& root = jsonBuffer.parseObject(dataControl[i]);
+    String device = root["LED"];
+    String state = root["STATUS"];
+    switch_device(device, state);
+    Serial.println("Turned " + state + " " + device + "!!!");
+  }
+  
+}
+
 void DoControl()
 {
   String data;
@@ -19,10 +53,12 @@ void DoControl()
       Serial.println(data);
     }
   }
-  memset(dataControl, '\0', 10);
+  for (i=0; i<10; i++) {
+    memset(&dataControl[i], '\0', dataControl[i].length());
+  }
   parseJson(data, '{', '}');
-  
   for (i=0; i<10 && dataControl[i].length() > 2; i++) {
+    Serial.println(dataControl[i]);
     JsonObject& root = jsonBuffer.parseObject(dataControl[i]);
     String device = root["LED"];
     String state = root["STATUS"];
@@ -35,15 +71,15 @@ void switch_device(String deviceName, String state)
 {
   int pin;
   int value;
-  if (deviceName == "light1") pin=12;
+  if (deviceName == "light1") pin=2;
   else if( deviceName == "light2") pin=13;
-  else if (deviceName == "hose1") pin=14;
-  else if (deviceName == "hose2") pin=15;
-  else if (deviceName == "sunshade1") pin=1;
-  else if (deviceName == "sunshade2") pin=3;
+  else if (deviceName == "hose1") pin=16;
+  else if (deviceName == "hose2") pin=14;
+  else if (deviceName == "sunshade1") pin=0;
+  else if (deviceName == "sunshade2") pin=12;
     
-  if(state == "On") value=HIGH;
-  else if(state == "Off") value=LOW;
+  if(state == "On") value=LOW;
+  else if(state == "Off") value=HIGH;
   
   digitalWrite(pin, value);
 }
